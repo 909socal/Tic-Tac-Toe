391433206036dc8e781ac51965ba6fddabb1c8fb
Gurjot Singh <Laugh_and_drive@Gurjots-MacBook-Pro.local>
2016-01-11 22:16:33 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,57 @@
+'use strict'
+
+var x = true;
+var o = false;
+var cnt =0; 
+
+$(document).ready(init);
+
+function init(){
+  $('.col-md-4').on('click' , turnHandler)
+  var x = true;
+}
+
+function turnHandler(event){
+  console.log($(this).text())
+		//console.log('turn handler');
+		if ( x === true){
+			$('h1').text("O's Turn");
+
+			console.log('Xs turn');
+			if($(this).text() !== 'O' && $(this).text() === '' ){
+				$(this).text('X');
+       x = false;
+       cnt++;
+     }
+
+   }
+   else{
+     console.log("Os turn ")
+     $('h1').text("X's Turn");
+     if($(this).text() !== 'X' && $(this).text() === '' ){
+      $(this).text('O');
+      x = true;
+      cnt ++;
+    }
+
+  }
+  winner();
+
+
+}
+
+function winner(){
+    if      ($('#bx1').text() === $('#bx2').text() && $('#bx2').text() !== '' && $('#bx2').text() === $('#bx3').text()) {alert($('#bx1').text() + "Are A Winner !!! ")}
+    else if ($('#bx1').text() === $('#bx4').text() && $('#bx4').text() !== '' && $('#bx4').text() === $('#bx7').text()) {alert($('#bx4').text() + "Are A Winner !!! ")}
+    else if ($('#bx1').text() === $('#bx5').text() && $('#bx5').text() !== '' && $('#bx5').text() === $('#bx9').text()) {alert($('#bx5').text() + "Are A Winner !!! ")}
+    else if ($('#bx4').text() === $('#bx5').text() && $('#bx5').text() !== '' && $('#bx5').text() === $('#bx6').text()) {alert($('#bx5').text() + "Are A Winner !!! ")}
+    else if ($('#bx7').text() === $('#bx8').text() && $('#bx8').text() !== '' && $('#bx8').text() === $('#bx9').text()) {alert($('#bx9').text() + "Are A Winner !!! ")}
+    else if ($('#bx2').text() === $('#bx5').text() && $('#bx5').text() !== '' && $('#bx5').text() === $('#bx8').text()) {alert($('#bx8').text() + "Are A Winner !!! ")}
+    else if ($('#bx3').text() === $('#bx6').text() && $('#bx6').text() !== '' && $('#bx6').text() === $('#bx9').text()) {alert($('#bx8').text() + "Are A Winner !!! ")}
+    else if ($('#bx3').text() === $('#bx5').text() && $('#bx5').text() !== '' && $('#bx5').text() === $('#bx7').text()) {alert($('#bx7').text() + "Are A Winner !!! ")}
+    else if (cnt >=9){alert(" Game Is A Tie!!!")}
+}
+
+
+
+
